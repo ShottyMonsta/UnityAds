16d47fd8b0bd392585bfcddf368c1a6caa472665
Thomas Cook <Thomas.cook@northumbria.ac.uk>
2016-08-14 12:32:39 +0100

    First Push
    Unity ads script for assignment

---
+++ Adverts.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class Adverts : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        Advertisement.Initialize ("33675", true);
+        StartCoroutine (ShowAdWhenReady());
+	}
+	
+	// Update is called once per frame
+    IEnumerator ShowAdWhenReady()
+    {
+        while (!Advertisement.IsReady())
+        {
+            yield return null;
+        }
+        Advertisement.Show();
+    }
+}
